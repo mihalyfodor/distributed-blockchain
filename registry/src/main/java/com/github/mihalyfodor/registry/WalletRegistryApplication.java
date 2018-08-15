c552d448c2a1834819ea09b5cfd5f4d4f6377d75
Mihaly Fodor <mihaly.fodor@outlook.com>
2018-08-15 10:49:46 +0300

    registry initialcommit

---
+++ registry/src/main/java/com/github/mihalyfodor/registry/WalletRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.github.mihalyfodor.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletRegistryApplication.class, args);
+    }
+}
